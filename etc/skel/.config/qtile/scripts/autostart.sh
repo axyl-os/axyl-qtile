f136d0c924ac9473eb0091268ac1236341715cef
Kungger-git <matthewriel63@gmail.com>
2021-10-12 08:11:53 +0800

    minor change in autostart script

--- etc/skel/.config/qtile/scripts/autostart.sh
+++ etc/skel/.config/qtile/scripts/autostart.sh
@@ -1,14 +1,14 @@
 #!/bin/env bash
 
-# Enable Super Keys For Menu
-ksuperkey -e 'Super_L=Alt_L|F1' &
-ksuperkey -e 'Super_R=Alt_L|F1' &
-
 # set background
 bash $HOME/.config/qtile/scripts/.fehbg
 
 # Kill if already running
 killall -9 picom xfce4-power-manager ksuperkey dunst sxhkd
+
+# Enable Super Keys For Menu
+ksuperkey -e 'Super_L=Alt_L|F1' &
+ksuperkey -e 'Super_R=Alt_L|F1' &
 
 # start hotkey daemon
 sxhkd &
@@ -18,7 +18,7 @@ dunst \
 -geom "280x50-10+38" -frame_width "1" -font "Source Code Pro Medium 10" \
 -lb "#0F131FFF" -lf "#82dbf4FF" -lfr "#548FABFF" \
 -nb "#0F131FFF" -nf "#82dbf4FF" -nfr "#548FABFF" \
--cb "#2E3440FF" -cf "#BF616AFF" -cfr "#BF616AFF" &
+-cb "#0F131FFF" -cf "#82dbf4FF" -cfr "#BF616AFF" &
 
 # power manager and picom start
 xfce4-power-manager &
